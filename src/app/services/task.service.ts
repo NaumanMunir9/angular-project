ac3197ebe88ab63bb98cada37d92274a8892875a
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-10-01 13:05:01 +0500

    task service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+// mock data
+import { TASKS } from '../mock-tasks';
+// interface
+import { Task } from '../Task';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor() {}
+
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
