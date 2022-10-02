a160406c8730afdbaa75cd14e799421d4d8178e4
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2022-10-02 23:08:47 +0500

    getTasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
+// libraries
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-// mock data
-import { TASKS } from '../mock-tasks';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 // interface
 import { Task } from '../Task';
 
@@ -9,10 +9,11 @@ import { Task } from '../Task';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
